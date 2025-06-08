519df2baa88a9e3fe74b6be5344e472be016a155
Lina Martinez <lmcastro3501@gmail.com>
2025-06-08 16:22:12 -0700

    Add elements to StudentList component and Add classes to it.

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,6 +1,15 @@
+import './StudentList.css';
+
 const StudentList = () => {
     return (
-        <h2>Student List</h2>
+        <section>
+            <h2 className='student-list__heading'>Student List</h2>
+            <ul className='student-list'>
+                <li>Student A</li>
+                <li>Student B</li>
+                <li>Student C</li>
+            </ul>
+        </section>
     );
 };
 
