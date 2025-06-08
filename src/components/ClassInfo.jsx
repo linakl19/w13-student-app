a46511e0b6bc772e843e98055a26fbe2d8343419
Lina Martinez <lmcastro3501@gmail.com>
2025-06-08 14:31:41 -0700

    Create and render ClassInfo component

---
+++ src/components/ClassInfo.jsx
@@ -0,0 +1,13 @@
+const ClassInfo = () => {
+    return (
+        <section>
+            <h1>Class Information</h1>
+            <ul>
+                <li>Name: Team Semicolons</li>
+                <li>Number of members: 30</li>
+            </ul>
+        </section>
+    );
+};
+
+export default ClassInfo;
