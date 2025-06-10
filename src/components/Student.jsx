0292016849a066a2752a7405691153b55864d339
Lina Martinez <lmcastro3501@gmail.com>
2025-06-10 09:25:10 -0700

    Add PropTypes and default Props to Student Component

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const Student = (props) => {
     return (
         <ul>
@@ -6,6 +8,16 @@ const Student = (props) => {
             <li>Email: {props.email}</li>
         </ul>
     );
+};
+
+Student.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string
+};
+
+// Using default props, we can make PropTypes optional instead of isRequired:
+Student.defaultProps = {
+    email: "unknown@example.com"
 };
 
 export default Student;
