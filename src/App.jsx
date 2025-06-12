315ce86e17135f100282eca33e78ee5fb8601a51
Lina Martinez <lmcastro3501@gmail.com>
2025-06-12 09:49:31 -0700

    Add state and toggleStudentPresence function to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import StudentList from './components/StudentList';
 import ClassInfo from './components/ClassInfo';
+import { useState } from 'react';
+
+const kInitialStudentData = [
+  {
+    id: 1,
+    nameData: 'Ada',
+    emailData: 'ada@dev.org',
+    isPresentData: false,
+  },
+  {
+    id: 2,
+    nameData: 'Soo-ah',
+    emailData: 'sooah@dev.org',
+    isPresentData: false,
+  },
+  {
+    id: 3,
+    nameData: 'Chrissy',
+    emailData: 'chrissy@dev.org',
+    isPresentData: true,
+  }
+];
 
 function App() {
-  const studentData = [
-    {
-      id: 1,
-      nameData: 'Ada',
-      emailData: 'ada@dev.org',
-      isPresentData: false,
-    },
-    {
-      id: 2,
-      nameData: 'Soo-ah',
-      emailData: 'sooah@dev.org',
-      isPresentData: false,
-    },
-    {
-      id: 3,
-      nameData: 'Chrissy',
-      emailData: 'chrissy@dev.org',
-      isPresentData: true,
-    }
-  ];
+  const [studentData, setStudentData] = useState(kInitialStudentData);
+
+const toggleStudentPresence = (studentId) => {
+    setStudentData(students => {
+      return students.map(student => {
+        if (student.id === studentId) {
+          return { ...student, isPresentData: !student.isPresentData };
+        } else {
+          return student;
+        }
+      });
+    });
+  };
 
   return (
     <main>
