2a750104cacf4898322fe258ba1147ed357f1c45
Lina Martinez <lmcastro3501@gmail.com>
2025-06-17 22:26:56 -0700

    Add PropTypes and event handler to ClassInfo component

--- src/components/ClassInfo.jsx
+++ src/components/ClassInfo.jsx
@@ -1,13 +1,22 @@
-const ClassInfo = (props) => {
+import PropTypes from 'prop-types';
+
+
+const ClassInfo = ({studentCount, onClickedDeleteAll}) => {
     return (
         <section>
             <h1>Class Information</h1>
             <ul>
                 <li>Name: Team Semicolons</li>
-                <li>Number of members: {props.studentCount}</li>
+                <li>Number of members: {studentCount}</li>
             </ul>
+            <button onClick={onClickedDeleteAll}>Delete All Students!</button>
         </section>
     );
+};
+
+ClassInfo.propTypes = {
+    studentCount: PropTypes.number.isRequired,
+    onClickedDeleteAll: PropTypes.func.isRequired,
 };
 
 export default ClassInfo;
